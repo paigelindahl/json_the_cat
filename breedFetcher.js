1cb0a5122ffaeaa98aeaecfb66d207d55f9c0910
paigelindahl <paigemarlindahl@gmail.com>
2020-09-24 21:22:13 +0000

    requested body from website depending on command line breed arguments

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,14 @@
+const args = process.argv.slice(2);
 const request = require('request');
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + args[0];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('Failed to make request');
+    process.exit;
+  }
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log('Breed request not found');
+  } else
+    console.log(data[0].description);
+});
